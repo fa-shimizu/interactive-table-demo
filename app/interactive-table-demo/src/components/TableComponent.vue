0a5e8ac2b712e3ed8347413f15eb325c7327a830
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2020-06-16 16:42:02 +0900

    編集可能

--- app/interactive-table-demo/src/components/TableComponent.vue
+++ app/interactive-table-demo/src/components/TableComponent.vue
@@ -3,12 +3,25 @@
     <table align="center">
       <thead>
         <tr>
-          <th v-for="column in columns" :key="column.field">{{ column.label }}</th>
+          <th
+            v-for="column in columns"
+            :key="column.field"
+            @click="setEditCell(null, null)"
+          >{{ column.label }}</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="row in rows" :key="row.id">
-          <td v-for="column in columns" :key="column.field">{{ row[column.field]}}</td>
+          <td
+            v-for="column in columns"
+            :key="column.field"
+            @click="setEditCell(row.id, column.field)"
+          >
+            <span v-if="!isEdit(row.id, column.field)">{{ row[column.field] }}</span>
+            <span v-else>
+              <input type="text" v-model="row[column.field]" />
+            </span>
+          </td>
         </tr>
       </tbody>
     </table>
@@ -40,49 +53,62 @@ export default {
       ],
       rows: [
         {
-          id: 1,
+          id: 0,
           name: "John",
           age: 20,
           createdAt: "",
           score: 0.03343
         },
         {
-          id: 2,
+          id: 1,
           name: "Jane",
           age: 24,
           createdAt: "2011-10-31",
           score: 0.03343
         },
         {
-          id: 3,
+          id: 2,
           name: "Susan",
           age: 16,
           createdAt: "2011-10-30",
           score: 0.3342
         },
         {
-          id: 4,
+          id: 3,
           name: "Chris",
           age: 55,
           createdAt: "2011-10-11",
           score: 0.41571
         },
         {
-          id: 5,
+          id: 4,
           name: "Dan",
           age: 40,
           createdAt: "2011-10-21",
           score: 0.9045
         },
         {
-          id: 6,
+          id: 5,
           name: "John",
           age: 20,
           createdAt: "2011-10-31",
           score: 0.02901
         }
-      ]
+      ],
+      editCell: {
+        id: null,
+        property: null
+      }
     };
+  },
+  methods: {
+    isEdit(id, property) {
+      const obj = { id, property };
+      return JSON.stringify(obj) === JSON.stringify(this.editCell);
+    },
+    setEditCell(id, property) {
+      this.editCell = { id, property };
+    }
   }
 };
 </script>
@@ -117,5 +143,13 @@ td:hover {
   background-color: #dcdcdc;
   text-decoration: underline;
   font-weight: bold;
+}
+
+input {
+  height: 32px;
+  width: 200px;
+  font-size: 24px;
+  padding: 0 8px;
+  text-align: center;
 }
 </style>
